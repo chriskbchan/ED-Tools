d100f7048bb6fea6180d87962ed73cc70326a2ca
Chris Chan <chris@chan.de.com>
2017-12-04 15:38:48 +0800

    add coriolis docking position function

--- ed.py
+++ ed.py
@@ -4,6 +4,18 @@ class System(object):
         self.name = _name
 
 class Station(object):
+    COR_PAD_CLOCK = [ 0,                                   # direction at N o'clock
+        6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 9, 9, 9, 9, 9, 10, 10, 10, 10, 11,    #  1 - 20
+        11, 11, 11, 12, 12, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4 ]   # 21 - 40
+    COR_PAD_DEPTH = [ 0,                                   # 1: Near, 2: Mid, 3: Far
+        1, 1, 3, 3, 1, 1, 2, 3, 1, 3, 1, 1, 2, 3, 3, 1, 1, 3, 3, 1,         #  1 - 20
+        1, 2, 3, 1, 3, 1, 1, 2, 3, 3, 1, 1, 3, 3, 1, 1, 2, 3, 1, 3 ]        # 21 - 40
+    COR_PAD_SIZE  = [ 0,                                   # 1: small, 2: medium, 3: large
+        1, 3, 2, 1, 2, 1, 2, 2, 3, 3, 2, 1, 1, 1, 2, 1, 3, 2, 1, 2,         #  1 - 20
+        1, 2, 2, 3, 3, 2, 1, 1, 1, 2, 1, 3, 2, 1, 2, 1, 2, 2, 3, 3 ]        # 21 - 40
+    PAD_DEPTH_DESC = [ "undefined", "near", "mid", "far" ]
+    PAD_SIZE_DESC  = [ "undefined", "small", "medium", "large" ]
+
     def __init__(self, _id, _name, _system_id, _type, _type_id, _is_planetary):
         self.id = _id
         self.name = _name
@@ -24,6 +36,9 @@ class Station(object):
         if self.type_id in [1,2,4,5,6,9,11,13]: # Outposts
             return 2
         return 0                                # Unknown
+
+    def coriolis_pad_info(self, pad_id):
+        return ( self.COR_PAD_CLOCK[pad_id], self.COR_PAD_DEPTH[pad_id], self.COR_PAD_SIZE[pad_id] )
 
     def print_info(self):
         if self.type:
--- mfd-tools.py
+++ mfd-tools.py
@@ -1,5 +1,6 @@
 from ed import *
 import json
+import random
 
 def load_systems(fn):
     print("Loading Systems...")
@@ -31,6 +32,14 @@ def find_station(stn_data, sys_data, station, system):
                             stn.set_system_name(sys.name)
                             return stn
 
+def show_landing_pad(station, pad_id):
+    pad_layout = station.docking_pad_layout()
+    if pad_layout == 1:
+        pad_info = station.coriolis_pad_info(pad_id)
+        print("Landing pad %d (%s) at %d o'clock %s" %
+            (pad_id, Station.PAD_SIZE_DESC[pad_info[2]], pad_info[0], Station.PAD_DEPTH_DESC[pad_info[1]]) )
+
+# main
 
 def main():
     system_data  = load_systems("systems_populated.json")
@@ -47,6 +56,7 @@ def main():
     for (stn, sys) in test_data:
         station = find_station(station_data, system_data, stn, sys)
         station.print_info()
+        show_landing_pad(station, random.randrange(40) + 1)
 
 if __name__ == "__main__":
     main()
